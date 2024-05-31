56f979a9e282a068b856ba38914ee0c74bb6bf7b
wangjian <wangjian@eazytec.com>
2024-05-31 10:32:49 +0800

    [optimize]

--- GHFollowers/Utilities/Constants.swift
+++ GHFollowers/Utilities/Constants.swift
@@ -18,6 +18,9 @@ enum SFSymbols {
 
 enum Images {
     static let ghLogo = UIImage(named: "gh-logo")
+    static let placeholder = UIImage(named: "avatar-placeholder")
+    static let emptyStateLogo = UIImage(named: "empty-state-logo")
+
 }
 
 enum ScreenSize {
